b224d56133c39b98a2d651292f66ae464ceb5e87
Anthony Gagnon <anthony.gagnon.2@ulaval.ca>
2019-12-15 19:41:32 -0500

    ajustemenbt interface'

--- test2.py
+++ test2.py
@@ -1,13 +1,32 @@
-import turtle
+état = {
+    "joueurs": [
+        {"nom": "idul", "murs": 7, "pos": [5, 5]}, 
+        {"nom": "automate", "murs": 3, "pos": [8, 6]}
+    ], 
+    "murs": {
+        "horizontaux": [[4, 4], [2, 6], [3, 8], [5, 8], [7, 8]], 
+        "verticaux": [[6, 2], [4, 4], [2, 6], [7, 5], [7, 7]]
+    }
+}
 
-#setup screen
-wn = turtle.Screen()
-wn.bgcolor("white")
+#organisation de la position des joueurs
+pos_x_1 = état["joueurs"][0]["pos"][0]      #position du joueur 1
+pos_y_1 = état["joueurs"][0]["pos"][1]      #position du joueur 1
+pos_x_2 = état["joueurs"][1]["pos"][0]      #position du joueur 2
+pos_y_2 = état["joueurs"][1]["pos"][1]      #position du joueur 2
+joueur1 = [1,pos_x_1,pos_y_1]
+joueur2 = [2,pos_x_2,pos_y_2]
 
-#setup player
-player1 = turtle.Turtle()
-player1.color("brown")
-player1.shape("circle")
-player1.penup()
+#organisation de la position des murs
+compte_murs_horiz = 0
+compte_murs_vert = 0
+for i in état["murs"]["horizontaux"]:
+    compte_murs_horiz += 1
+for i in état["murs"]["verticaux"]:
+    compte_murs_vert += 1
 
-turtle.done()
+pos_murs_horiz = état["murs"]["horizontaux"][compte_murs_horiz - 1]
+pos_murs_vert = état["murs"]["verticaux"][compte_murs_vert - 1]
+murs_horiz = ["h", pos_murs_horiz]
+murs_vert = ["v", pos_murs_vert]
+print(murs_vert)
